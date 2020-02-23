8692536b489728ba72d208add8af288777b536d1
Mark <ratrateroo@gmail.com>
2020-02-24 03:38:41 +0800

    updated the square component

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+  render() {
+    return (
+      <button className="square" onClick={function() { alert('click'); }}>
+        {this.props.value}
+      </button>
+    );
   }
+}
   
   class Board extends React.Component {
     renderSquare(i) {
